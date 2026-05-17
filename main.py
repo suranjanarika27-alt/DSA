100b8f272580c073c52c3715e77d7a5492f3b1a4
suranjana <suranjanarika27@gmail.com>
2026-05-18 01:00:18 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+def binarySearch(arr, target):
+    low = 0
+    high = len(arr) - 1
+    mid = (low + high) // 2
+    while low <= high:
+        if arr[mid] == target:
+            print(f"Element found at index: {mid}")
+            return
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+        mid = (low + high) // 2
+    print("Element not found in the array.")
+    return 
+
+if __name__ == "__main__":
+    target = int(input("Enter the target:"))
+    arr = list(map(int, input("Enter the array:").split()))
+    arr.sort()
+    print("Sorted array:", arr)
+    binarySearch(arr, target)
+
+
+#"1"-->integer-->1
+#1 2 3 4 5--> "1 2 3 4 5"--> "1", "2", "3", "4", "5"--> 1, 2, 3, 4, 5 --> [1, 2, 3, 4, 5]
